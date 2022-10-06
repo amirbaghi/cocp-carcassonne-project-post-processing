02688ad3051a6deb44c86022f2342f87337b720b
amirbaghi <bamirmasoud@gmail.com>
2022-10-06 11:58:41 +0200

    add unfinished image rotation code

---
+++ rotated_image_generator.py
@@ -0,0 +1,6 @@
+from PIL import Image
+
+def rotateImage(image):
+    rotatedImages = [image]
+    for rotation in [Image.ROTATE_90, Image.ROTATE_180, Image.ROTATE_270]:
+        # rotatedImages += image.
